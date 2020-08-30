70dc33b37538fbaaae20223b9c4d3db4dbd793e5
Wesley Monk <wmonk@student.neumont.edu>
2020-08-30 17:21:05 -0600

    Revert "Merge pull request #12 from BrandinGoldsberry/OnePersonEnd"
    This reverts commit 960d5ddefcb49b580ab528a1ab15cb2e1b0832b7, reversing
    changes made to 151d9f11af62f6cd09b64868b0329159ee355075.

--- Uno/Properties/Resources.Designer.cs
+++ Uno/Properties/Resources.Designer.cs
@@ -223,26 +223,6 @@ namespace Uno.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap bsa {
-            get {
-                object obj = ResourceManager.GetObject("bsa", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap bt {
-            get {
-                object obj = ResourceManager.GetObject("bt", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap CardTable {
             get {
                 object obj = ResourceManager.GetObject("CardTable", resourceCulture);
@@ -436,26 +416,6 @@ namespace Uno.Properties {
         internal static System.Drawing.Bitmap gs {
             get {
                 object obj = ResourceManager.GetObject("gs", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap gsa {
-            get {
-                object obj = ResourceManager.GetObject("gsa", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap gt {
-            get {
-                object obj = ResourceManager.GetObject("gt", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
@@ -683,26 +643,6 @@ namespace Uno.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap rsa {
-            get {
-                object obj = ResourceManager.GetObject("rsa", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap rt {
-            get {
-                object obj = ResourceManager.GetObject("rt", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap StartupWindow {
             get {
                 object obj = ResourceManager.GetObject("StartupWindow", resourceCulture);
@@ -876,26 +816,6 @@ namespace Uno.Properties {
         internal static System.Drawing.Bitmap ys {
             get {
                 object obj = ResourceManager.GetObject("ys", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap ysa {
-            get {
-                object obj = ResourceManager.GetObject("ysa", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap yt {
-            get {
-                object obj = ResourceManager.GetObject("yt", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
